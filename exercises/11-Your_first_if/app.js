324c18c229a62370fa1e6c3513523ccfe36c1baf
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-08-26 20:51:49 +0000

    Working on exercise 11

--- exercises/11-Your_first_if/app.js
+++ exercises/11-Your_first_if/app.js
@@ -5,9 +5,9 @@ if (total>100) {
     console.log ("We still have a bit of driving left to go")
 }
 
-else.if (total>100 && total <100) {
+else if(total>50 && total <100) {
     console.log ("We'll be there in 5 minutes")
 }
-else. if (total <=50 ) {
+else if (total <=50 ) {
     console.log ("I' am parking. I'see you right now")
 }

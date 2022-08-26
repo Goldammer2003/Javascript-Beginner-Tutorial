3aea3232cbd8d1c31703f19b1a0e9a7efd3b2d2d
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-08-26 20:22:44 +0000

    Still working on Ecercise 11

--- exercises/11-Your_first_if/app.js
+++ exercises/11-Your_first_if/app.js
@@ -2,12 +2,12 @@ var total = prompt('How many km are left to go?');
 
 // Your code below:
 if (total>100) {
-    console.log "We still have a bit of driving left to go"
+    console.log ("We still have a bit of driving left to go")
 }
 
 else.if (total>100 && total <100) {
-    console.log "We'll be there in 5 minutes"
+    console.log ("We'll be there in 5 minutes")
 }
 else. if (total <=50 ) {
-    console.log "I' am parking. I'see you right now"
+    console.log ("I' am parking. I'see you right now")
 }
